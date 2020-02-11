4eb1d8b001d77459518af594f47d2bcd2a97469d
Danny Meyer <Danny.Meyer@gmail.com>
2020-02-10 19:22:27 -0500

    updates readme

--- test.py
+++ test.py
@@ -5,6 +5,6 @@ chroma, _, sr, _ = create_chroma("daft.wav")
 time_time_similarity = TimeTimeSimilarityMatrix(chroma, sr)
 time_lag_similarity = TimeLagSimilarityMatrix(chroma, sr)
 
-## Visualize the results
-#time_time_similarity.display()
-#time_lag_similarity.display()
+# Visualize the results
+# time_time_similarity.display()
+# time_lag_similarity.display()
